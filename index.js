b8451c51144845d377d2cfb0ef2583df1f364118
Vasyl Holubovskyi <holvascompany@gmail.com>
2024-08-07 23:51:04 +0200

    ---up/fixed index---

--- index.js
+++ index.js
@@ -1,87 +1,55 @@
-const TelegramApi = require('node-telegram-bot-api') //імпортуємо модуль node-telegram-bot-api
-const {gameOptions, againOptions} = require('./options')
-// const sequelize = require('./db');
-// const UserModel = require('./models');
-
+const TelegramApi = require('node-telegram-bot-api'); //імпортуємо пакет
+const  {verseOptions, againOptions} = require('./options');
 const token = '7354137351:AAHNnMPsKk7oqbavuZ8R2TJe2xdjBo4sIHY'; //токен взаїмодії з ботом
+//текстові повідомлення
+const bot = new TelegramApi(token, {polling: true}); 
+const chats = {};
 
-const bot = new TelegramApi(token, {polling: true})
-
-const chats = {}
-
-
-const startGame = async (chatId) => {
-    await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен ее угадать!`);
-    const randomNumber = Math.floor(Math.random() * 10)
+const startChoose = async (chatId) => {
+    await bot.sendMessage(chatId, 'Обери число від 0 до 10');
+    const randomNumber = Math.floor(Math.random() * 10);
     chats[chatId] = randomNumber;
-    await bot.sendMessage(chatId, 'Отгадывай', gameOptions);
+    await bot.sendMessage(chatId, 'Напиши оберане число', verseOptions);
 }
 
-//встановлення команд бота по api
-const start = async () => {
-
-    // try {
-    //     await sequelize.authenticate()
-    //     await sequelize.sync()
-    // } catch (e) {
-    //     console.log('Подключение к бд сломалось', e)
-    // }
-
+const start = () => {
+    //встановлення команд бота по api
     bot.setMyCommands([
         {command: '/start', description: 'Привітання'},
-        {command: '/info', description: 'Інформація про користувача'},
-        {command: '/game', description: 'Гра: вгадай цифру'},
-    ])
+        {command: '/info', description: 'Інфо про користувача'},
+        {command: '/game', description: 'Оримати вірш'},
 
+    ]) 
     //вішаємо слухача на обробку отриманних повідомлень
     bot.on('message', async msg => {
-        const text = msg.text;
-        const chatId = msg.chat.id;
-
-        try {
-            if (text === '/start') {
-                // await UserModel.create({chatId})
-                await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp')
-                return bot.sendMessage(chatId, `${msg.from.first_name}, вітаю тебе в BlessDay боті.`);
-            }
-            if (text === '/info') {
-                // const user = await UserModel.findOne({chatId})
-                return bot.sendMessage(chatId, `Ім'я: ${msg.from.first_name} ${msg.from.last_name}, в грі у тебе правильних відповідей ... , а неправильних ... `); //${user.right}, ${user.wrong} 
-            }
-            if (text === '/game') {
-                return startGame(chatId);
-            }
-            return bot.sendMessage(chatId, 'Я тебе не розумію! Будь ласка, обери дію в МЕНЮ');
-        } catch (e) {
-            return bot.sendMessage(chatId, 'Виникла якась помилка!');
+        const text = msg.text; //отримання повідомлень
+        const chatId = msg.chat.id;  
+        
+        if (text === '/start') {
+            await bot.sendMessage(chatId, `https://tlgrm.eu/_/stickers/0cc/ba1/0ccba11f-e506-3c8c-8862-a4d914dcf683/2.jpg`); //відправка повідомлень
+            return bot.sendMessage(chatId, `Привіт, ${msg.from.first_name}! Тебе вітає BlessDay бот. Тут ти отримуватимеш щоденне благословіння з Божого Слова`);
         }
-
-    })
+        if (text === '/info') {
+            return bot.sendMessage(chatId, `Тебе звуть ${msg.from.first_name} ${msg.from.last_name}! Твій нік ${msg.from.username}`);
+        }
+        if (text === '/game') {
+            return startChoose(chatId);
+        }
+        return bot.sendMessage(chatId, 'Я не зрозумів. Будь ласка, обери дію в МЕНЮ');
+    }); 
 
     bot.on('callback_query', async msg => {
         const data = msg.data;
         const chatId = msg.message.chat.id;
         if (data === '/again') {
-            return startGame(chatId)
+            return startChoose(chatId);
         }
-        // const user = await UserModel.findOne({chatId})
-        if (data == chats[chatId]) {
-            // user.right += 1;
-            await bot.sendMessage(chatId, `Вітаю, ти відгадав(-ла) цифру ${chats[chatId]}`, againOptions);
+        if (data === chats[chatId]) {
+            return await bot.sendMessage(chatId, `Вітаю! Ви вгадали число ${chats[chatId]}!`, againOptions);
         } else {
-            // user.wrong += 1;
-            await bot.sendMessage(chatId, `Нажаль ти не відгадав(-ла), я загадав цифру ${chats[chatId]}`, againOptions);
+            return await bot.sendMessage(chatId, `На жаль, ви не вгадали. Я загадував число ${chats[chatId]}.`, againOptions);
         }
-        // await user.save();
     })
 }
 
-start()
-
-
-        // // Cron job для регулярних повідомлень
-        // cron.schedule('0 8-21 * * *', async () => {
-        //     const chatId = 'YOUR_CHAT_ID'; // Використовуйте ваш чат ID
-        //     const verse = 'Here is your hourly Bible verse!';
-        //     await bot.sendMessage(chatId, verse);
-        // });
+start();
