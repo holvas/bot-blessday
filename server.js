0cfd057b05294166c232c6416c07edbba2e10ec2
Vasyl Holubovskyi <holvascompany@gmail.com>
2024-08-08 09:38:45 +0200

    add chalk

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const chalk = require('chalk');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const errorMsg = chalk.bgKeyword('white').redBright;
+const successMsg = chalk.bgKeyword('green').white;
+
 // Прив'язка до порту
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, (error) => {
+  error ? console.log(errorMsg(error)) : console.log(successMsg(`Server is running on port ${PORT}`));
 });
