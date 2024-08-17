0fc5c9e8d9a8d96881ab0e9d0580cde37ff649bc
Vasyl Holubovskyi <holvascompany@gmail.com>
2024-08-17 22:19:52 +0200

    add user & saves data function

--- index.js
+++ index.js
@@ -18,64 +18,73 @@ const startChoose = async (chatId) => {
 }
 
 const start = async () => {
-    try { 
-        await connectDB(); //Підключення до бази даних
-    } catch (err) {
-        console.log('Підключення до БД не відбулось :(', err);
-        return;
-    }
+        try { 
+            await connectDB(); //Підключення до бази даних
+        } catch (err) {
+            console.log('Підключення до БД не відбулось :(', err);
+            return;
+        }
 
 
-    //встановлення команд бота по api
-    bot.setMyCommands([
-        {command: '/start', description: 'Привітання'},
-        {command: '/info', description: 'Інфо про користувача'},
-        {command: '/game', description: 'Оримати вірш'},
+        //встановлення команд бота по api
+        bot.setMyCommands([
+            {command: '/start', description: 'Привітання'},
+            {command: '/info', description: 'Інфо про користувача'},
+            {command: '/game', description: 'Оримати вірш'},
 
-    ]) 
-    //вішаємо слухача на обробку отриманних повідомлень
-    bot.on('message', async msg => {
-        const text = msg.text; //отримання повідомлень
-        const chatId = msg.chat.id;  
-        
-        try {
-            if (text === '/start') {
-                await User.create({chatId});
-                await bot.sendMessage(chatId, `https://tlgrm.eu/_/stickers/0cc/ba1/0ccba11f-e506-3c8c-8862-a4d914dcf683/2.jpg`); //відправка повідомлень
-                return bot.sendMessage(chatId, `Привіт, ${msg.from.first_name}! Тебе вітає BlessDay бот. Тут ти отримуватимеш щоденне благословіння з Божого Слова`);
+        ]) 
+        //вішаємо слухача на обробку отриманних повідомлень
+        bot.on('message', async msg => {
+            const text = msg.text; //отримання повідомлень
+            const chatId = msg.chat.id;  
+            
+            try {
+                if (text === '/start') {
+
+                    const existingUser = await User.findOne({ chatId });
+                    if (!existingUser) {
+                        await User.create({ chatId });
+                    }
+                    await bot.sendMessage(chatId, `https://tlgrm.eu/_/stickers/0cc/ba1/0ccba11f-e506-3c8c-8862-a4d914dcf683/2.jpg`); //відправка повідомлень
+                    return bot.sendMessage(chatId, `Привіт, ${msg.from.first_name}! Тебе вітає BlessDay бот. Обери бажану дію в меню.`);
+                }
+                if (text === '/info') {
+                    const user = await User.findOne({chatId});
+                    return bot.sendMessage(chatId, `${msg.from.first_name}, у тебе в грі правильних відповідей ${user.right}, неправильних відповідей ${user.wrong}`);
+                }
+                if (text === '/game') {
+                    return startChoose(chatId);
+                }
+                return bot.sendMessage(chatId, 'Я не зрозумів. Будь ласка, обери дію в МЕНЮ');
+            } catch (err) {
+                return bot.sendMessage(chatId, 'Виникла помилочка ooops!')
             }
-            if (text === '/info') {
-                const user = await User.findOne({chatId});
-                return bot.sendMessage(chatId, `Тебе звуть ${msg.from.first_name} ${msg.from.last_name}/ У тебе в грі правильних відповідей ${User.right}, неправильних відповідей ${User.wrong}`);
-            }
-            if (text === '/game') {
+        }); 
+
+        bot.on('callback_query', async msg => {
+            const data = msg.data;
+            const chatId = msg.message.chat.id;
+            if (data === '/again') {
                 return startChoose(chatId);
             }
-            return bot.sendMessage(chatId, 'Я не зрозумів. Будь ласка, обери дію в МЕНЮ');
-        } catch (err) {
-            return bot.sendMessage(chatId, 'Виникла помилочка ooops!')
-        }
-    }); 
 
-    bot.on('callback_query', async msg => {
-        const data = msg.data;
-        const chatId = msg.message.chat.id;
-        if (data === '/again') {
-            return startChoose(chatId);
-        }
-        if (data === chats[chatId]) {
-            return await bot.sendMessage(chatId, `Вітаю! Ви вгадали число ${chats[chatId]}!`, againOptions);
-        } else {
-            return await bot.sendMessage(chatId, `На жаль, ви не вгадали. Я загадував число ${chats[chatId]}.`, againOptions);
-        }
-    })
+            const user = await User.findOne({chatId});
 
+            if (data === chats[chatId]) {
+                user.right += 1;
+                await bot.sendMessage(chatId, `Вітаю! Ви вгадали число ${chats[chatId]}!`, againOptions);
+            } else {
+                user.right += 1;
+                await bot.sendMessage(chatId, `На жаль, ви не вгадали. Я загадував число ${chats[chatId]}.`, againOptions);
+            }
+            await user.save();
+        })
 
-    cron.schedule('*/25 * * * *', async () => {
-        // const verse = 'Here is your Bible verse!';
-        // bot.sendMessage(chatId, verse);
-        return await bot.sendMessage(chatId, `Тссс ${msg.from.first_name}! Почитаємо Біблію?`, againOptions);
-    });
+        // cron.schedule('*/25 * * * *', async () => {
+        //     // const verse = 'Here is your Bible verse!';
+        //     // bot.sendMessage(chatId, verse);
+        //     return await bot.sendMessage(chatId, `Тссс ${msg.from.first_name}! Почитаємо Біблію?`, againOptions);
+        // });
     }
 
 start();
